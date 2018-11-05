3d284954e368b6a61f7bc2b5802327a2eac0637c
Artem Kariavka <artem@kariavka.com>
2018-11-05 17:32:47 +0200

    [*] Updated Homepage UI

--- app/routes/news/item.js
+++ app/routes/news/item.js
@@ -1,4 +1,23 @@
 import Route from '@ember/routing/route';
+import {inject as service} from '@ember/service';
+import {get, set} from '@ember/object';
+import {hash} from 'rsvp';
 
 export default Route.extend({
+  // Services
+  store: service(),
+
+  // Model
+  model(params) {
+    const store = get(this, 'store');
+
+    return hash({
+      item: store.findRecord('post', params.id, {reload: true}),
+    });
+  },
+
+  afterModel(model) {
+    const title = get(model, 'item.title');
+    set(this, 'title', title);
+  },
 });
